c52c15f3a96fe97bc122e528bac8eb2913f84e94
serhSK <sergeyskotnitskiy@gmail.com>
2017-12-17 03:25:02 +0200

    added ability to get data on the list of authors or one author

--- ht8_parser.py
+++ ht8_parser.py
@@ -1,5 +1,6 @@
 import csv
 import requests
+import argparse
 from bs4 import BeautifulSoup
 
 
@@ -15,6 +16,10 @@ def write_dict_to_csv(csv_file, csv_columns, dict_data):
         writer.writerow(dict_data)
 
 
+args_parser = argparse.ArgumentParser()
+args_parser.add_argument('-l', action='store', nargs='+', dest='authors', default=[])
+args = args_parser.parse_args()
+
 quotes_to_scrape_url = "http://quotes.toscrape.com"
 page_number = 1
 has_next = True
@@ -27,26 +32,26 @@ while has_next:
     quotes = soup.select("div.quote")
     if len(quotes) > 0:
         for quote in quotes:
-            quote_text = quote.select("span.text")[0].text[1:-1]
-            author_url = quote.select("span > a")[0].get("href")
             author_title = quote.select("span > small")[0].text
-            author_page = requests.get(quotes_to_scrape_url + author_url)
-            author_soup = BeautifulSoup(author_page.text, "html.parser")
-            author_born_date = author_soup.select("span.author-born-date")[0].text
-            author_born_place = author_soup.select("span.author-born-location")[0].text[3:]
-            author_about = author_soup.select("div.author-description")[0].text.strip()
-            quote_tags = quote.select("a.tag")
-            result_tag_str = ""
-            for tag in quote_tags:
-                tag_name = tag.text
-                tag_url = tag.get("href")
-                result_tag_str += tag_separator.join((tag_name, tag_url)) + "\n"
-            csv_data = {"Quote": quote_text, "Author": author_title, "AuthorUrl": author_url,
-                        "AuthorBornDate": author_born_date, "AuthorBornPlace": author_born_place,
-                        "Tags": result_tag_str,
-                        "AboutAuthor": author_about}
-            write_dict_to_csv("dump.csv", columns, csv_data)
+            if author_title.replace(" ", "-") in args.authors or len(args.authors) == 0:
+                quote_text = quote.select("span.text")[0].text[1:-1]
+                author_url = quote.select("span > a")[0].get("href")
+                author_page = requests.get(quotes_to_scrape_url + author_url)
+                author_soup = BeautifulSoup(author_page.text, "html.parser")
+                author_born_date = author_soup.select("span.author-born-date")[0].text
+                author_born_place = author_soup.select("span.author-born-location")[0].text[3:]
+                author_about = author_soup.select("div.author-description")[0].text.strip()
+                quote_tags = quote.select("a.tag")
+                result_tag_str = ""
+                for tag in quote_tags:
+                    tag_name = tag.text
+                    tag_url = tag.get("href")
+                    result_tag_str += tag_separator.join((tag_name, tag_url)) + "\n"
+                csv_data = {"Quote": quote_text, "Author": author_title, "AuthorUrl": author_url,
+                            "AuthorBornDate": author_born_date, "AuthorBornPlace": author_born_place,
+                            "Tags": result_tag_str,
+                            "AboutAuthor": author_about}
+                write_dict_to_csv("dump.csv", columns, csv_data)
     else:
         has_next = False
     page_number += 1
-
