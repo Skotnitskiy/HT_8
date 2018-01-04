8e6092933c41c281039fd913c84147b9eac4d308
serhSK <sergeyskotnitskiy@gmail.com>
2018-01-04 17:23:06 +0200

    get shuffled numbers list

--- domains.py
+++ domains.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+
 def get_pages_numbers(pages=2025, records_per_page=25):
     numbers = list(range(0, pages, records_per_page))
+    np.random.shuffle(numbers)
     return numbers
