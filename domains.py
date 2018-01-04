f25afe8dfafd7d9f962667cc388ea9d4232af770
serhSK <sergeyskotnitskiy@gmail.com>
2018-01-04 18:43:38 +0200

    get array for each one proxy

--- domains.py
+++ domains.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 
-def get_pages_numbers(pages=2025, records_per_page=25):
-    numbers = list(range(0, pages, records_per_page))
+def get_pages_numbers():
+    pages = 2000
+    records_per_page = 25
+    proxies = 10
+    numbers = np.array(range(0, pages, records_per_page))
     np.random.shuffle(numbers)
+    numbers = np.hsplit(numbers, proxies)
+    last = np.append(numbers[0], pages)  # add last element to array
+    numbers[0] = last
     return numbers
